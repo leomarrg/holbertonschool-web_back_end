46ca28e271c77efa67dd7e36de510bad849259da
leomar <leomar.rodriguez1@upr.edu>
2024-04-26 14:15:36 -0400

    sums an input list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Takes input list and returns the sum"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns sum of a list"""
+    return sum(input_list)
