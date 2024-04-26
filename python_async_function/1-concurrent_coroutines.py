a8d4bb1655da70b86d2e00122990fff099beec26
leomar <leomar.rodriguez1@upr.edu>
2024-04-26 15:02:58 -0400

    fixed errors on task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""async function"""
-from typing import List
+"""concurrent coroutines"""
 import asyncio
+from typing import List
 
-arandwait = __import__('0-basic_async_syntax').wait_random
+randwait = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
