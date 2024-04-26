f571a329ed0c79947cdf0fc485f4784aa8269d96
leomar <leomar.rodriguez1@upr.edu>
2024-04-26 14:48:39 -0400

    added readme file

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,4 +8,5 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
+    """returns int arg eventually"""
     return delay
