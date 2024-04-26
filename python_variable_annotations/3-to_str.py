4b41ad24e453fda3f191118f127e5f3f3b4d8abd
leomar <leomar.rodriguez1@upr.edu>
2024-04-26 14:09:13 -0400

    takes a num float and returns the string representation

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function takes float and returns thr string 
+    representation"""
+
+
+def to_str(n: float) -> str:
+    """returns string representation"""
+    return str(n)
